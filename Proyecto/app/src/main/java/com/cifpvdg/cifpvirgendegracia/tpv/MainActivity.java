a39c3caa2dad1a49e36719776fb4cf42b24fa0e7
Unknown <luis.shadow.esp@gmail.com>
2019-02-07 09:36:24 +0100

    Version funcional escaner (Modificar relacion de aspecto)

--- Proyecto/app/src/main/java/com/cifpvdg/cifpvirgendegracia/tpv/MainActivity.java
+++ Proyecto/app/src/main/java/com/cifpvdg/cifpvirgendegracia/tpv/MainActivity.java
@@ -1,14 +1,71 @@
 package com.cifpvdg.cifpvirgendegracia.tpv;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.cifpvdg.cifpvirgendegracia.tpv.Escaner.CameraPreviewActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Permission List
+    private String[] REQUEST_PERMISSIONS = new String[]{Manifest.permission.CAMERA};
+    // Permission Request Code
+    private int RESULT_PERMISSIONS = 0x9000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.escaner_barra);
-        //Esto es una prueba
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_show_preview_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isPermissionGranted()) startCameraPreviewActivity();
+            }
+        });
+    }
+
+    /** Go to camera preview */
+    private void startCameraPreviewActivity(){
+        startActivity(new Intent(this, CameraPreviewActivity.class));
+    }
+
+    /** Request permission and check */
+    private boolean isPermissionGranted(){
+        int sdkVersion = Build.VERSION.SDK_INT;
+        if(sdkVersion >= Build.VERSION_CODES.M) {
+            //Android6.0(Marshmallow)
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, REQUEST_PERMISSIONS, RESULT_PERMISSIONS);
+                return false;
+            } else {
+                return true;
+            }
+        }else{
+            //Android6.0(Marshmallow)
+            return true;
+        }
+    }
+
+    /** Post-process for granted permissions */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (RESULT_PERMISSIONS == requestCode) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
+                startCameraPreviewActivity();
+            } else {
+
+            }
+            return;
+        }
     }
 }
