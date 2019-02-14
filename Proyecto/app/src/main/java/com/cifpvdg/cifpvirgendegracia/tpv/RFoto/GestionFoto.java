332671f94f68a67dadbbd98e315b867eb10d3c8e
DK <joshuamaro@gmail.com>
2019-02-14 09:42:49 +0100

    Subir Foto
    Codigo para subir imagen

--- Proyecto/app/src/main/java/com/cifpvdg/cifpvirgendegracia/tpv/RFoto/GestionFoto.java
+++ Proyecto/app/src/main/java/com/cifpvdg/cifpvirgendegracia/tpv/RFoto/GestionFoto.java
@@ -1,4 +1,140 @@
 package com.cifpvdg.cifpvirgendegracia.tpv.RFoto;
 
-public class GestionFoto {
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+public class GestionFoto extends AppCompatActivity implements View.OnClickListener{
+    private Button btnBuscar;
+    private Button btnSubir;
+    private ImageView imageView;
+    private int PICK_IMAGE_REQUEST = 1;
+    private Bitmap bitmap;
+    private String UPLOAD_URL ="https://tpvdam2.000webhostapp.com/upload.php";
+    private String KEY_IMAGEN = "pifot";
+    private String KEY_NOMBRE = "pid";
+    int CAPTURA_IMAGEN_THUMBNAIL = 99,CAPTURA_IMAGEN_TAMAÑO_REAL=30;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnBuscar = (Button)findViewById(R.id.bCaptura);
+        btnSubir = (Button)findViewById(R.id.bCon);
+        btnSubir.setEnabled(false);
+        imageView  = (ImageView) findViewById(R.id.imageView2);
+        btnBuscar.setOnClickListener(this);
+        btnSubir.setOnClickListener(this);
+        //cargarFoto();
+    }
+    public String getStringImagen(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void uploadImage(){
+        //Mostrar el diálogo de progreso
+        final ProgressDialog loading = ProgressDialog.show(this,"Subiendo...","Espere por favor...",false,false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPLOAD_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        //Descartar el diálogo de progreso
+                        loading.dismiss();
+                        //Mostrando el mensaje de la respuesta
+                        Toast.makeText(GestionFoto.this, s , Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        //Descartar el diálogo de progreso
+                        loading.dismiss();
+                        //Showing toast
+                        Toast.makeText(GestionFoto.this, volleyError.getMessage().toString(), Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //Convertir bits a cadena
+                String imagen = getStringImagen(bitmap);
+                //Obtener el nombre de la imagen
+                String nombre = "25";
+                //Creación de parámetros
+                Map<String,String> params = new Hashtable<String, String>();
+
+                //Agregando de parámetros
+                params.put(KEY_IMAGEN, imagen);
+                params.put(KEY_NOMBRE, nombre);
+
+                //Parámetros de retorno
+                return params;
+            }
+        };
+        //Creación de una cola de solicitudes
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        //Agregar solicitud a la cola
+        requestQueue.add(stringRequest);
+    }
+
+    private void showFileChooser() {
+        /*Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Imagen"), PICK_IMAGE_REQUEST);*/
+        Intent hacerFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (hacerFotoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(hacerFotoIntent, CAPTURA_IMAGEN_THUMBNAIL);
+            //dispatchTakePictureIntent();
+            //galleryAddPic();
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURA_IMAGEN_THUMBNAIL && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            bitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == btnBuscar){
+            btnSubir.setEnabled(true);
+            showFileChooser();
+        }
+
+        if(v == btnSubir){
+            uploadImage();
+        }
+    }
+
 }
